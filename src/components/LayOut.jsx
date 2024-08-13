935ded8b80e45cfa9b1c2841b4bfe02e853861ad
Talha <talhaiftikhar12549@gmail.com>
2024-08-13 10:27:04 +0500

    Added API in LayOUt

--- src/components/LayOut.jsx
+++ src/components/LayOut.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import NavBar from "./NavBar.jsx";
-import { Outlet } from "react-router-dom";
+import {Outlet} from "react-router-dom";
 import Footer from "./Footer";
+import {useDispatch} from "react-redux";
+import {useEffect} from "react";
+import axios from "axios";
+import {apiData} from "../redux/slice/counterslice.js"
 
 export default function Layout() {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://newsdata.io/api/1/news?apikey=pub_491000e07b3fbeecd7423939dd410845c437d&q=Computer Science');
+                dispatch(apiData(response.data.results));
+                console.log("Data dispatched from App");
+            } catch (e) {
+                console.error('API Not Working', e);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+
     return (
         <div className="flex flex-col justify-between h-[100vh]">
-            <NavBar />
-            <Outlet />
+            <NavBar/>
+            <Outlet/>
             <Footer></Footer>
         </div>
     );
